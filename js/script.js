bc7a8d9f5c0b34660e33665eb259c0c16b6ee025
ana <ana@blueberry.ie>
2023-10-11 16:18:04 +0100

    typed words & welcome section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 /* dropdown menu */
+
 function showDropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
@@ -18,11 +19,53 @@ function showDropdown() {
   }
 
 
+  
+
+/* intro animated text */
+    var words = ['development.', 'design.'],
+    wordWrapper = document.getElementById('typed-words'),
+    wordWrapperContent = wordWrapper.innerHTML,
+    addingWord = false,
+    counter = 0;
+
+function animateWords() {
+      setInterval(function () {
+        if (wordWrapperContent.length > 0 && !addingWord) {
+          wordWrapper.innerHTML = wordWrapperContent.slice(0, -1);
+          wordWrapperContent = wordWrapper.innerHTML;
+        } else {
+          addingWord = true;
+        }
+    
+        if (addingWord) {
+          if (wordWrapperContent.length < words[counter].length) {
+            wordWrapper.innerHTML = words[counter].slice(0, wordWrapperContent.length + 1);
+            wordWrapperContent = wordWrapper.innerHTML;
+          } else {
+            if (counter < words.length - 1) {
+              counter++;
+            } else {
+              counter = 0; // Reset counter to repeat words
+            }
+            addingWord = false;
+          }
+        }
+      }, 150);
+    }
+    
+// Call the function to start the animation
+animateWords();
+
+
+
+
+
+
 /* button on scroll */
 
 mybutton = document.getElementById("back-to-top");
 
-// When the user scrolls down 20px from the top of the document, show the button
+// if user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -33,10 +76,10 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
+//on click, scroll to the top of the document
 function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.body.scrollTop = 0; // Safari
+  document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
 }
 
 jQuery(document).ready(function ($) {
